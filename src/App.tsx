7e05682cddbb32fa06bbdd2d96d7c05bbe03acd4
Toshiro Kubota <toshiro732@hotmail.com>
2025-05-28 19:21:24 -0400

    almost ready

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import './App.css'
 import AcknowledgementCard from './components/AcknowledgementCard';
 import RatingForm from './components/RatingForm';
-import Header from './components/Header';
 
 function App() {
   const [rating, setRating] = useState<number>(0);
--- src/components/RatingForm.tsx
+++ src/components/RatingForm.tsx
@@ -3,10 +3,12 @@ import { staticAsset } from "../lib";
 
 export default function RatingForm({setRating}: {setRating: React.Dispatch<React.SetStateAction<number>>}) {
     const [ratingLocal, setRatingLocal] = useState<number>(0);
+    const [error, setError] = useState<boolean>(false);
 
     function clickHandler(event: React.MouseEvent<HTMLInputElement>) {
         const target = event.target as HTMLInputElement;
         setRatingLocal(Number(target.value));
+        setError(false);
         console.log('clickHandler', target.value)
     }
 
@@ -14,6 +16,8 @@ export default function RatingForm({setRating}: {setRating: React.Dispatch<React
         event.preventDefault();
         if(ratingLocal > 0) {
             setRating(ratingLocal);
+        } else {
+            setError(true);
         }
     }
     return (
@@ -24,33 +28,36 @@ export default function RatingForm({setRating}: {setRating: React.Dispatch<React
         <h1>
             How did we do?
         </h1>
-        <p>
-            Please let us know how we did with your support request. All feedback is appreciated 
-            to help us improve our offering!
-        </p>
+        <div>
+            <p>
+                Please let us know how we did with your support request. All feedback is appreciated 
+                to help us improve our offering!
+            </p>
+            <p className={`error-msg ${error ? '':'opacity-0'}`}>Please rate us!</p>
+        </div>
           <div className='rating-buttons flex align-center justify-between'>
-            <input type="radio" id="rating1" name="rating" value="1" onClick={clickHandler} />
             <label htmlFor="rating1" className="circular-shade">
+                <input type="radio" id="rating1" name="rating" value="1" onClick={clickHandler} />
                 1
             </label>
-            <input type="radio" id="rating2" name="rating" value="2" onClick={clickHandler} />
             <label htmlFor="rating2" className="circular-shade">
+                <input type="radio" id="rating2" name="rating" value="2" onClick={clickHandler} />
                 2
             </label>
-            <input type="radio" id="rating3" name="rating" value="3" onClick={clickHandler} />
             <label htmlFor="rating3" className="circular-shade">              
+                <input type="radio" id="rating3" name="rating" value="3" onClick={clickHandler} />
                 3
             </label>
-            <input type="radio" id="rating4" name="rating" value="4" onClick={clickHandler} />
             <label htmlFor="rating4" className="circular-shade">
+                <input type="radio" id="rating4" name="rating" value="4" onClick={clickHandler} />
                 4
             </label>
-            <input type="radio" id="rating5" name="rating" value="5" onClick={clickHandler} />
             <label htmlFor="rating5" className="circular-shade">
+                <input type="radio" id="rating5" name="rating" value="5" onClick={clickHandler} />
                 5
             </label>
           </div>
-          <button  onClick={formHandler}>Submit</button>
+          <button onClick={formHandler}>Submit</button>
       </form>
     )
 }
